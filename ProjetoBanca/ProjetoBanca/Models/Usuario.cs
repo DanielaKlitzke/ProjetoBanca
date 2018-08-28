df951f1492d97e50f16d2dae3357077d9b567d87
Daniela Klitzke <kochdaniela.historia@gmail.com>
2018-08-28 17:41:48 -0300

    DAO

---
+++ ProjetoBanca/ProjetoBanca/Models/Usuario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoBanca.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public String Nome { get; set; }
+        public String CpfOuCnpj { get; set; }
+        public String Rg { get; set; }
+        public String Sexo { get; set; }
+        public String Endereco { get; set; }
+        public String Bairro { get; set; }
+        public int NumeroResidencia { get; set; }
+        public String Complemento { get; set; }
+        public String Cidade { get; set; }
+        public String Estado { get; set; }
+        public String Cep { get; set; }
+        public int Telefone { get; set; }
+        public String DataNascimento { get; set; }
+        public String TipoPessoa { get; set; }
+        public String Senha { get; set; }
+        public String Email { get; set; }
+    }
+}
