19025b2af77537896dfca94c626f44b313971059
m008 <m008@benner.com.br>
2018-09-10 08:56:38 -0300

    Categoria

---
+++ ProjetoBanca/ProjetoBanca/Controllers/CategoriaController.cs
@@ -0,0 +1,34 @@
+﻿using ProjetoBanca.DAO;
+using ProjetoBanca.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoBanca.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        // GET: Cadastro
+        public ActionResult Index()
+        {
+            CategoriaDAO dao = new CategoriaDAO();
+            IList<Categoria> categorias = dao.ListarCategoria();
+            ViewBag.Categoria = categorias;
+            return View();
+        }
+
+        public ActionResult Form()
+        {
+            return View();
+        }
+
+        public ActionResult Adiciona(Categoria categoria)
+        {
+            CategoriaDAO dao = new CategoriaDAO();
+            dao.Adicionar(categoria);
+            return RedirectToAction("Index", "Categoria");
+        }
+    }
+}
