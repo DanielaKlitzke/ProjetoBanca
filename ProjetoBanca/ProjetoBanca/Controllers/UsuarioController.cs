5f94a14c996c4887e0f09aaa59bba09c92060dd0
m008 <m008@benner.com.br>
2018-09-13 09:42:55 -0300

    UsuarioController

---
+++ ProjetoBanca/ProjetoBanca/Controllers/UsuarioController.cs
@@ -0,0 +1,27 @@
+﻿using ProjetoBanca.DAO;
+using ProjetoBanca.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoBanca.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            UsuarioDAO dao = new UsuarioDAO();
+            IList<Usuario> usuarios = dao.ListarUsuario();
+            ViewBag.Usuario = usuarios;
+            return View();
+        }
+
+        public ActionResult Form()
+        {
+            return View();
+        }
+    }
+}
